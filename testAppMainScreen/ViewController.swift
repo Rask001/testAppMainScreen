2dc321d90a89ac29c7b3ca394525a847b450c391
Anton Prokopev <97259692+Rask001@users.noreply.github.com>
2022-10-13 13:45:53 +0300

    Initial Commit

---
+++ testAppMainScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testAppMainScreen
+//
+//  Created by Антон on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
